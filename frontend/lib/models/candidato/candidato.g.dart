8062954ba62d6ca8ae4b61957ca473e64c3219ad
JacoNeto <jaconeto8@gmail.com>
2021-11-15 00:21:01 -0300

    models added

---
+++ frontend/lib/models/candidato/candidato.g.dart
@@ -0,0 +1,21 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'candidato.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Candidato _$CandidatoFromJson(Map<String, dynamic> json) => Candidato(
+      nome: json['nome'] as String?,
+      numero: json['numero'] as int?,
+      partido: json['partido'] as String?,
+      cargo: json['cargo'] as int?,
+    );
+
+Map<String, dynamic> _$CandidatoToJson(Candidato instance) => <String, dynamic>{
+      'nome': instance.nome,
+      'numero': instance.numero,
+      'partido': instance.partido,
+      'cargo': instance.cargo,
+    };
